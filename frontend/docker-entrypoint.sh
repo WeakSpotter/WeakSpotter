926639f1b105ca2c226b748c043500ae9e792556
Ozeliurs <ozeliurs@gmail.com>
2024-11-18 13:36:00 +0100

    fix(docker-entrypoint.sh): properly escape API_URL variable in config.js to prevent syntax errors

--- frontend/docker-entrypoint.sh
+++ frontend/docker-entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-echo "window.__APP_CONFIG__ = { API_URL: "$API_URL" };" > /usr/share/nginx/html/config.js
+echo "window.__APP_CONFIG__ = { API_URL: \"$API_URL\" };" > /usr/share/nginx/html/config.js
 
 nginx -g "daemon off;"
