8b69821c480321706094d9b3e5ce79dd46410170
Ozeliurs <ozeliurs@gmail.com>
2024-12-20 21:38:36 +0100

    refactor(sublist3r.py): modernize Sublist3rJob class to conform to abstract_job interface

--- backend/app/jobs/unit_jobs/recon/sublist3r.py
+++ backend/app/jobs/unit_jobs/recon/sublist3r.py
@@ -1,47 +1,41 @@
-import docker
-from app.jobs.container import run_container
-from app.jobs.job import Job
-from app.models.scan import Scan
 import re
+
+from app.jobs.abstract_job import Job
+from app.jobs.container import run_container
 
 
 class Sublist3rJob(Job):
-    def __init__(self):
-        super().__init__("Sublist3r", "sublist3r", self.perform_scan)
+    requirements = ["domain"]
+    key = "sublist3r"
+    name = "Sublist3r Scan"
 
-    @staticmethod
-    def perform_scan(scan: Scan):
+    def run(self):
         "Scan a domain with sublist3r."
-        domain = scan.data_dict.get("domain")
+        domain = self._scan.data_dict.get("domain")
 
-        if not domain:
-            print("Aucun domain specifié.")
-            return None
+        self._raw_output = run_container(
+            "ghcr.io/weakspotter/sublist3r:latest", f"-d {domain}"
+        )
 
-        try:
-            result = run_container(
-                "ghcr.io/weakspotter/sublist3r:latest", f"-d {domain}"
-            )
-            return Sublist3rJob.parse_sublist3r_output(result)
-        except docker.errors.DockerException as e:
-            print(f"Erreur lors de l'éxecution du conteneur Docker: {e}")
-            return None
-
-    @staticmethod
-    def parse_sublist3r_output(output: str) -> dict:
-        "Parse the output of sublist3r and remove ANSI escape codes."
-        findings = []
-        append_lines = False
+    def parse_results(self):
+        """Parse the output of sublist3r and extract subdomains."""
+        self.result = []
         ansi_escape = re.compile(
-            r"\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])"
+            r"\x1B[@-_][0-?]*[ -/]*[@-~]"
         )  # Pattern to match ANSI codes
-        for line in output.splitlines():
-            if "Total Unique Subdomains Found:" in line:
-                append_lines = True
-                continue
-            if append_lines:
-                # Remove ANSI escape codes and add to findings
-                clean_line = ansi_escape.sub("", line).strip()
-                findings.append(clean_line)
+        subdomains_section = False
 
-        return {"sublist3r_findings": findings}
+        for line in self._raw_output.splitlines():
+            if "Total Unique Subdomains Found:" in line:
+                subdomains_section = True
+                continue
+            if subdomains_section:
+                clean_line = ansi_escape.sub("", line).strip()
+                if clean_line:  # Ensure the line is not empty
+                    self.result.append(clean_line)
+
+    def score(self):
+        return 0.0
+
+    def definitions(self):
+        return {}
