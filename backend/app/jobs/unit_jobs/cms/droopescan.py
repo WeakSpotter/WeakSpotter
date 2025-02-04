1a04929b5c2d0ab303fd64faaf7135be01179a2e
amandinemart1 <amandine-martin@sfr.fr>
2025-02-04 14:48:40 +0100

    addition begininn droppscan (pb import imp)

---
+++ backend/app/jobs/unit_jobs/cms/droopescan.py
@@ -0,0 +1,28 @@
+from app.jobs.abstract_job import Job
+from app.jobs.container import run_container
+from app.jobs.license import License
+
+
+class DroopescanJob(Job):
+    requirements = []
+    key = "droopescan"
+    name = "Droopescan"
+    license = License.GPLv2
+
+    def run(self) :
+        "Scan csm site with droopescan."
+        url = self._scan.url
+        self.result = {}
+
+        self._raw_output = run_container(
+            "ghcr.io/weakspotter/droopescan:latest", f"scan drupal -u {url}"
+        )
+
+    def parse_results(self) -> None:
+        self.result = self._raw_output
+
+    def score(self) :
+        return 0.0
+    
+    def definitions(self):
+        return {}
