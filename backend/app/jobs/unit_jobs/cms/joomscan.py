ddaacd9cccf496bea498423b6ab337cb009220cf
amandinemart1 <amandine-martin@sfr.fr>
2025-02-04 10:48:10 +0100

    addition jobs joomscan

---
+++ backend/app/jobs/unit_jobs/cms/joomscan.py
@@ -0,0 +1,26 @@
+from app.jobs.abstract_job import Job
+from app.jobs.license import License
+from app.jobs.container import run_container
+
+class JoomscanJob(Job):
+    requirements = []
+    key = "joomscan"
+    name = "Joomscan"
+    license = License.GPLv3
+
+    def run(self) :
+        "Scan csm site with joomscan."
+        url = self._scan.url
+        self.result = {}
+
+        self._raw_output = run_container(
+            "docker pull ghcr.io/weakspotter/joomscan:latest", f"-u {url}"
+        )
+    def parse_results(self) -> None:
+        self.result = self._raw_output
+    
+    def score(self) :
+        return 0.0
+    
+    def definitions(self):
+        return {}
