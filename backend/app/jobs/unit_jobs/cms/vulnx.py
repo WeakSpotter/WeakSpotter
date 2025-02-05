8eca67992170db7fe591b6b80ff139e3145372dc
amandinemart1 <amandine-martin@sfr.fr>
2025-02-05 10:11:03 +0100

    addition VulnX container and execution with parser

---
+++ backend/app/jobs/unit_jobs/cms/vulnx.py
@@ -0,0 +1,44 @@
+from app.jobs.abstract_job import Job
+from app.jobs.license import License
+from app.jobs.container import run_container
+
+import re
+
+class VulnXJob(Job):
+    requirements = []
+    key = "vulnx"
+    name = "VulnX"
+    license = License.GPLv3
+
+    def run(self):
+        "Scan CMS site with VulnX."
+        url = self._scan.url
+        self.result = {}
+
+        # Run the command in the Docker container
+        self._raw_output = run_container(
+            "ghcr.io/weakspotter/vulnx:latest", f"--url {url}"
+        )
+
+    def parse_results(self):
+        # Nettoyer la sortie brute en supprimant les séquences d'échappement ANSI
+        clean_output = re.sub(r'\x1b\[[0-9;]*m', '', self._raw_output)
+    
+        # Séparer la sortie en lignes
+        section = clean_output.split('\n')
+    
+        # Filtrer les lignes contenant des crochets [ ] et supprimer leur contenu
+        filtered_lines = [
+            re.sub(r'\[.*?\]', '', line).strip()  # Supprime le contenu entre [ ] et nettoie les espaces
+            for line in section
+            if '[' in line and ']' in line
+        ]
+    
+        # Stocker le résultat filtré
+        self.result = filtered_lines
+
+    def score(self):
+        return 0.0
+    
+    def definitions(self):
+        return {}
